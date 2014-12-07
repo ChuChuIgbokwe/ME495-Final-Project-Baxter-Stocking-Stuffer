4142a7b7ddf975d7179326a5b9299025bbcdfa24
Chu-Chu <raider64x@gmail.com>
2014-12-06 19:23:59 -0600

    work in progress

--- src/stocking_position_locator.py
+++ src/stocking_position_locator.py
@@ -48,9 +48,103 @@ movement = 0 #QR code movement (0,1,2)
 
 tag_id = int()
 
+scanned_ar = None
+
 #Create publisher to send PoseStamped() message to topic for Baxter to move towards
-pub_cd = rospy.Publisher('start/colordetection', Bool)
+pub_cd = rospy.Publisher('start/colordetection', Bool) #change to true once you get the stocking position
 pub_stocking_pose = rospy.Publisher('stocking_pose',PoseStamped)
+pub_move = rospy.Publisher('/baxter_movement/posestamped',PoseStamped)
+
+#initially move to hard coded position
+def MovetoFirstStocking():
+	'''
+	Move to the first stocking. It's position can either be hard coded or input using a private parameter
+	'''
+    move_to_pose = PoseStamped()
+    move_to_pose.header=Header(stamp=rospy.Time.now(), frame_id='base')
+    move_to_pose.pose.position=Point(
+                    x=pointx,# hard code the posiiton and orientation of the first stocking here
+                    y=pointy,
+                    z=pointz,
+                )
+    move_to_pose.pose.orientation=Quaternion(
+                    x=quatx,# and here
+                    y=quaty,
+                    z=quatz,
+                    w=quatw,
+                )
+	pub_move.publish(move_to_pose)
+
+def MovetoQRcode():
+	'''
+	Combine position and orientation in a PoseStamped() message
+	'''
+    move_to_pose = PoseStamped()
+    move_to_pose.header=Header(stamp=rospy.Time.now(), frame_id='base')
+    move_to_pose.pose.position=Point(
+                    x=pose_ee[0,0],
+                    y=pose_ee[1,0],
+                    z=pose_ee[2,0],	#add the distance between the QR code and the stocking here
+                )
+    move_to_pose.pose.orientation=Quaternion(
+                    x=pose_ee[3,0],
+                    y=pose_ee[4,0],
+                    z=pose_ee[5,0],
+                    w=pose_ee[6,0],
+                )
+	pub_move.publish(move_to_pose)
+
+	
+def MovetoSecondStocking():
+	move_to_pose = PoseStamped()
+    move_to_pose.header=Header(stamp=rospy.Time.now(), frame_id='base')
+    move_to_pose.pose.position=Point(
+                    x=pose_ee[0,0],
+                    y=pose_ee[1,0], # we're moving in the -ve y direction however far the stockings are placed
+                    z=pose_ee[2,0],	#whatever we moved up before, we're moving down
+                )
+    move_to_pose.pose.orientation=Quaternion(
+                    x=pose_ee[3,0],
+                    y=pose_ee[4,0],
+                    z=pose_ee[5,0],
+                    w=pose_ee[6,0],
+                )
+	pub_move.publish(move_to_pose)
+		
+def ComparePresentIDtoScannedID(currentstocking):
+	if scanned_ar == tag_id:
+		#Store pose of QR code from camera into local variables
+		#PoseStamp messages contains a header and a pose. We care only about the pose so we extract it.
+		position_visp = tag_msg.pose.position
+		quat_visp = tag_msg.pose.orientation
+
+		#rospy.loginfo("Tag Point Position: [ %f, %f, %f ]"%(position_visp.x, position_visp.y, position_visp.z))
+		#rospy.loginfo("Tag Quat Orientation: [ %f, %f, %f, %f]"%(quat_visp.x, quat_visp.y, quat_visp.z, quat_visp.w))
+
+		tag_pos_x = position_visp.x
+		tag_pos_y = position_visp.y 
+		tag_pos_z = position_visp.z
+
+		tag_quat_x = quat_visp.x
+		tag_quat_y = quat_visp.y
+		tag_quat_z = quat_visp.z
+		tag_quat_w = quat_visp.w
+
+
+		move_to_pose = PoseStamped()
+		move_to_pose.header=Header(stamp=rospy.Time.now(), frame_id='base')
+		move_to_pose.pose.position=Point(
+		                x=tag_pos_x,
+		                y=tag_pos_y, #we move to the ar code on the next stocking 
+		                z=tag_pos_z,	
+		            )
+		move_to_pose.pose.orientation=Quaternion(
+		                x=tag_quat_x,
+		                y=tag_quat_y,
+		                z=tag_quat_z,
+		                w=tag_quat_w,
+		            )
+		
 
 #Gets pose of end-effector from Baxter
 def getPoseEE(msg):
@@ -192,6 +286,10 @@ def sweep_val(msg):
     global run
     run = msg.data
 
+def read_ar(msg):
+	global scanned_ar
+	for m in msg.markers:
+        scanned_ar = m.id
 
 def main():
     rospy.init_node('create_pose_using_qr_code',anonymous = True)
@@ -201,11 +299,13 @@ def main():
     rospy.Subscriber("/visp_auto_tracker/object_position",PoseStamped,getPoseTag)
     rospy.Subscriber("/sweep",Bool,sweep_val)
     rospy.Subscriber("/scanned_stocking_id",Int8,get_tag_id)
+	rospy.Subscriber("/ar_pose_marker",AlvarMarkers,read_ar)
+
 
     #Wait for left gripper's end effector pose
     while not first_flag:
         pass
-
+	MovetoFirstStocking()
 
     #Continously running to send PoseStamped() message to Baxter
     while not rospy.is_shutdown():
